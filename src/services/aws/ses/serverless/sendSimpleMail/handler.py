8ac9a35dd2f6d99a67df1ee87155319ec87e0dda
Felipe Sales <soufeliposales@gmail.com>
2024-02-25 23:04:43 -0300

    feat: add method to call aws ses via lambda

--- src/services/aws/ses/serverless/sendSimpleMail/handler.py
+++ src/services/aws/ses/serverless/sendSimpleMail/handler.py
@@ -1,30 +1,47 @@
-import json
+import boto3
+import os
 
-def sendMail(event, context):
+ses = boto3.client('ses')
 
-    # put comment here
+def send_mail(
+    sender_name: str,
+    sender_email: str,
+    subject: str,
+    body: str
+):
+    sender = f"{sender_name} <{sender_email}>"
 
-    """
-    This function sends a simple email using SES
-    """
-
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+    message = {
+        'Template': 'simple-email',
+        'Source': sender,
+        'Destination': {
+            'ToAddresses': [
+                os.environ.get('AWS_SES_EMAIL_RECEIVER', '')
+            ]
+        },
+        # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ses/client/send_templated_email.html
+        'TemplateData': {
+            'subject': subject,
+            'body': body
+        }
     }
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    try:
+        response = ses.send_templated_email(
+            Source=message['Source'],
+            Destination=message['Destination'],
+            Template=message['Template'],
+            TemplateData=message['TemplateData']
+        )
+        print('Email sent successfully:', response)
+    except Exception as e:
+        print('Error sending email:', e)
 
-    return response
+send_mail(
+    sender_name="Felipe",
+    sender_email="lfelipedecs@gmail.com",
+    subject="More one email",
+    body="This is a simple email"
+)
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+print("All done!")
