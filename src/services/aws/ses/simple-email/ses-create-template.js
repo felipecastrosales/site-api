d7e88c997ebcc10e87b2aa4c0300a0566d3c2271
Felipe Sales <soufeliposales@gmail.com>
2024-04-07 22:13:59 -0300

    chore: added draft template

--- src/services/aws/ses/simple-email/ses-create-template.js
+++ src/services/aws/ses/simple-email/ses-create-template.js
@@ -15,7 +15,28 @@ const sesClient = new SESClient(SESConfig);
 const run = async (templateName) => {
     const date = new Date();
 
-    const createTemplateCommand = new CreateTemplateCommand({
+    // const createTemplateCommand = new CreateTemplateCommand({
+    //     Template: {
+    //         TemplateName: templateName,
+    //         SubjectPart: `Soliciatação enviada com sucesso`,
+    //         HtmlPart: `
+    //             Olá, {{ username }}! <br>
+    //             <p>Recebi sua solicitação com sucesso e em breve retornarei com uma resposta.</p>
+    //             <p>Atenciosamente,</p>
+    //             <p>Felipe Sales</p>
+    //             <p>Sent at ${date}</p>  
+    //         `,
+    //         TextPart: `
+    //             Olá, {{ username }}!
+    //             Recebi sua solicitação com sucesso e em breve retornarei com uma resposta.
+    //             Atenciosamente,
+    //             Felipe Sales
+    //             Sent at ${date}
+    //         `,
+    //     },
+    // });
+
+    const updateTemplateCommand = new UpdateTemplateCommand({
         Template: {
             TemplateName: templateName,
             SubjectPart: `{{ subject }}`,
@@ -32,23 +53,6 @@ const run = async (templateName) => {
         },
     });
 
-    // const updateTemplateCommand = new UpdateTemplateCommand({
-    //     Template: {
-    //         TemplateName: templateName,
-    //         SubjectPart: `{{ subject }}`,
-    //         HtmlPart: `
-    //             <p>User name: {{ username }} | User email: {{ userEmail }}</p>
-    //             <p>{{ body }}</p>
-    //             <p>Sent at ${date}</p>  
-    //         `,
-    //         TextPart: `
-    //             User name: {{ username }} | User email: {{ userEmail }}
-    //             {{ body }}
-    //             Sent at ${date}
-    //         `,
-    //     },
-    // });
-
     try {
         const result = await sesClient.send(createTemplateCommand);
         console.log('Template created: \n', result);
@@ -57,4 +61,5 @@ const run = async (templateName) => {
     }
 };
 
+// run('request-site-simple-mail');
 run('site-simple-mail');
